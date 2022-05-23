e808b2366e6eb3e60311ab272c7f064998b2a1fe
Tovintsoa <tovintsoa1337@capri-nc.com>
2022-05-23 19:27:50 +0300

    Rectif

--- app/src/main/java/com/m1p9/kidz/ui/registration/RegistrationActivity.java
+++ app/src/main/java/com/m1p9/kidz/ui/registration/RegistrationActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.m1p9.kidz.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private static Button btn_inscription;
+    private static EditText nomPrenom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
