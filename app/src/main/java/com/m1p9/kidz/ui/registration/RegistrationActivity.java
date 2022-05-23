0a10f9da86388d46419b5ec37188206b9d1bf65e
Tovintsoa <tovintsoa1337@capri-nc.com>
2022-05-23 18:57:47 +0300

    Inscription

--- app/src/main/java/com/m1p9/kidz/ui/registration/RegistrationActivity.java
+++ app/src/main/java/com/m1p9/kidz/ui/registration/RegistrationActivity.java
@@ -1,7 +1,10 @@
 package com.m1p9.kidz.ui.registration;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.m1p9.kidz.R;
@@ -12,5 +15,11 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#FF6200EE"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+
     }
 }
